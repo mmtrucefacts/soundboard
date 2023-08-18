6e840b842842bbec511eae5146b7eafbaf3cb5ce
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2023-08-17 19:15:06 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,4 @@
-alert("When clicking a sound, please wait for the website to fully load all of the sounds or nothing will happen. If you are stuipd enough to spam all of the sounds at once, just press the Stop Everything button on the bottom-left of the screen. Please do not spam click any of the buttons.");
+
+function myFunction() {
+  alert("When clicking a sound, please wait for the website to fully load all of the sounds or nothing will happen. If you are stuipd enough to spam all of the sounds at once, just press the Stop Everything button on the bottom-left of the screen. Please do not spam click any of the buttons.")
+}
